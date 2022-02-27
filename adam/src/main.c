67ca7d70352f83f7d225b72b4358199310c2cab2
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2022-02-27 16:54:55 -0600

    bifurcate into adam and atari

---
+++ adam/src/main.c
@@ -0,0 +1,16 @@
+/**
+ * Weather 
+ *
+ * Based on @bocianu's code
+ *
+ * @author Thomas Cherryhomes
+ * @email thom dot cherryhomes at gmail dot com
+ *
+ */
+
+#include "screen.h"
+
+void main(void)
+{
+  screen_test();
+}
---
+++ adam/src/screen.h
@@ -0,0 +1,12 @@
+/**
+ * Weather
+ *
+ * Screen routines
+ */
+
+#ifndef SCREEN_H
+#define SCREEN_H
+
+void screen_test(void);
+
+#endif /* SCREEN_H */
